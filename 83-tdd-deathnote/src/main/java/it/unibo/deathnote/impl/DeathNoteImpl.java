411aefcb1af127a161c223e32020d7774b365a14
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2023-11-26 22:31:51 +0100

    es 83: classe di testing completata, creato "DeathNoteImpl"

---
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,67 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote {
+
+    @Override
+    public String getRule(int ruleNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRule'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeName(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean writeDeathCause(String cause) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean writeDetails(String details) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDeathCause(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDeathDetails(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isNameWritten(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    }
+    
+}
--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,102 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
 class TestDeathNote {
 
+    private DeathNote deathNote;
+    private String human;
+    private String otherHuman;
+
+    @BeforeEach
+    public void setUp() {
+        this.deathNote = new DeathNoteImpl();
+        this.human = "Joe Bartolozzi";
+        this.otherHuman = "Gerry Biscotti";
+    }
+
+    @Test
+    public void testRuleGetter() {
+        assertThrows(IllegalArgumentException.class, () -> deathNote.getRule(0));
+        assertThrows(IllegalArgumentException.class, () -> deathNote.getRule(-1));
+        assertThrows(IllegalArgumentException.class, () -> deathNote.getRule(DeathNote.RULES.size()));
+        assertDoesNotThrow(() -> deathNote.getRule(1));
+        try {
+            deathNote.getRule(-1);
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+            assertFalse(e.getMessage().isEmpty());
+        }
+    }
+
+    @Test
+    public void testRuleList() {
+        for (String rule: DeathNote.RULES) {
+            assertNotNull(rule);
+            assertFalse(rule.isBlank());
+            assertFalse(rule.isEmpty());
+        }
+    }
+
+    @Test
+    public void testNameWriting() {
+        assertFalse(deathNote.isNameWritten(human));
+        deathNote.writeName(human);
+        assertTrue(deathNote.isNameWritten(human));
+        assertFalse(deathNote.isNameWritten(otherHuman));
+        assertThrows(NullPointerException.class, () -> deathNote.writeName(null));
+        // ???
+        assertThrows(NullPointerException.class, () -> deathNote.writeName(""));
+        assertThrows(NullPointerException.class, () -> deathNote.writeName(" "));
+    }
+
+    @Test
+    public void testDeathCause() {
+        assertThrows(IllegalStateException.class, () -> deathNote.writeDeathCause(null));
+        assertThrows(IllegalStateException.class, () -> deathNote.writeDeathCause("cause with no name written"));
+        deathNote.writeName(human);
+        assertEquals("heart attack", deathNote.getDeathCause(human));
+        deathNote.writeName(otherHuman);
+        assertTrue(deathNote.writeDeathCause("karting accident"));
+        assertEquals("karting accident", deathNote.getDeathCause(otherHuman));
+        waitSomeTime(100);
+        assertFalse(deathNote.writeDeathCause("ate without youtube"));
+        assertEquals("karting accident", deathNote.getDeathCause(otherHuman));
+    }
+
+    @Test
+    public void testDeathDetails() {
+        assertThrows(IllegalStateException.class, () -> deathNote.writeDetails("sas"));
+        assertThrows(IllegalStateException.class, () -> deathNote.writeDetails(null));
+        assertThrows(IllegalStateException.class, () -> deathNote.writeDetails(""));
+        assertThrows(IllegalStateException.class, () -> deathNote.writeDetails(" "));
+        deathNote.writeName(human);
+        assertEquals("", deathNote.getDeathDetails(human));
+        assertTrue(deathNote.writeDetails("ran for too long"));
+        assertEquals("ran for too long", deathNote.getDeathDetails(human));
+        deathNote.writeName(otherHuman);
+        waitSomeTime(2000);
+        assertFalse(deathNote.writeDetails("3 secs with no zaza"));
+        assertEquals("", deathNote.getDeathDetails(otherHuman));
+    }
+
+    private void waitSomeTime(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
